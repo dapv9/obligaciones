6522be30f54e7acea4b9ed63c8dfa4835ce8519a
David Parra <dapv9@hotmail.com>
2023-12-22 17:06:48 -0500

    se actualiza como retorna los datos

--- api-obligaciones.py
+++ api-obligaciones.py
@@ -17,12 +17,12 @@ def inicio():
 # ruta para obtener información del DataFrame con las obligaciones de los clientes
 @app.route('/api/obligaciones', methods=['GET'])
 def get_olbigaciones():
-    return Response(df_olbigaciones.to_string(index=False,sep=';'),content_type='text/plain')
+    return Response(df_obligaciones.to_csv(index=False,sep=';'),content_type='text/plain')
 
 # ruta para obtener información del DataFrame de los clientes con el valor total de sus obligaciones
 @app.route('/api/clientes', methods=['GET'])
 def get_clientes():
-    return Response(df_cliente_total.to_string(index=False,sep=';'),content_type='text/plain')
+    return Response(df_cliente_total.to_csv(index=False,sep=';'),content_type='text/plain')
 
 if __name__ == '__main__':
     app.run(debug=True)
