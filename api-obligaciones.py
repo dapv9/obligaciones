c69fdf2904178ed1353560451a3139487728f8aa
David Parra <dapv9@hotmail.com>
2023-12-22 16:41:28 -0500

    se actualiza descripcion de codigo

--- api-obligaciones.py
+++ api-obligaciones.py
@@ -9,12 +9,17 @@ df_cliente_total = pd.read_pickle('df_total_cliente.pkl')
 #se crea instancia de flask
 app = Flask(__name__)
 
-# Endpoint para obtener información del primer DataFrame
+# Ruta por defecto
+@app.route('/', methods=['GET'])
+def inicio():
+    return 'api para consultar obligaciones y clientes \n Obligaciones: /api/olbigaciones \n Clientes: /api/clientes'
+
+# ruta para obtener información del DataFrame con las obligaciones de los clientes
 @app.route('/api/olbigaciones', methods=['GET'])
 def get_olbigaciones():
     return jsonify(df_olbigaciones.to_dict(orient='records'))
 
-# Endpoint para obtener información del segundo DataFrame
+# ruta para obtener información del DataFrame de los clientes con el valor total de sus obligaciones
 @app.route('/api/clientes', methods=['GET'])
 def get_clientes():
     return jsonify(df_cliente_total.to_dict(orient='records'))
